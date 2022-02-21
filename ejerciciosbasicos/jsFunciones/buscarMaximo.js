07b0137337d6463efd6464611162570dbeea79f0
Diana <dianabarrio@hotmail.com>
2022-02-21 15:46:01 +0100

    ejercicios

---
+++ ejerciciosbasicos/jsFunciones/buscarMaximo.js
@@ -0,0 +1,12 @@
+/*1.Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+    // insert code
+  }*/
+  function sum(num1, num2) {
+    if (num1 > num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
---
+++ ejerciciosbasicos/jsLoops/probandoforof.js
@@ -0,0 +1,14 @@
+/*Usa un bucle forof para recorrer todos los destinos del array. Imprime en un console.log sus valores.
+onst placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']*/
+const placesToTravel = [
+    "Japon",
+    "Venecia",
+    "Murcia",
+    "Santander",
+    "Filipinas",
+    "Madagascar",
+  ];
+  
+  for (const place of placesToTravel) {
+    console.log(place);
+  }
